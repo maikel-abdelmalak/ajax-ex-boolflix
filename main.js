a3a3fa62df9e307879573745db8b11990eb7a24b
maikel abdelmalak <maikel22@hotmail.it>
2020-05-22 15:56:25 +0200

    .

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
 $(document).ready(function(){
 
-$('.search button').on('click', cerca_film ()
-)
+$('.search button').on('click', cerca_film)
 
 
 
 function cerca_film (){
-    var ricerca = 'batman'
+    var ricerca = $('.search input').val()
+    if(ricerca != ''){
 
-    $.ajax({
+        $.ajax({
 
-        'url': 'https://api.themoviedb.org/3/search/movie',
-        'method': 'GET',
-        'data': {
-            'api_key': 'ca530b2f29c17ae553941132a3b631ce',
-            'query': ricerca,
-        },
-        success: function(data){
-            var film = data.results
-            for (var i = 0; i < film.length; i++) {
-                var stampa = film[i].title;
-                console.log(stampa);
+            'url': 'https://api.themoviedb.org/3/search/movie',
+            'method': 'GET',
+            'data': {
+                'api_key': 'ca530b2f29c17ae553941132a3b631ce',
+                'query': ricerca,
+            },
+            success: function(data){
+                var film = data.results
+                for (var i = 0; i < film.length; i++) {
+                    var titolo = film[i].title;
+                    var titoloOriginale = film[i].title;
+                    var lingua= film[i].title;
+                    var voto = film[i].title;
+                    console.log(stampa);
+                }
+
+            },
+            error: function(a, b, c){
+                var numero = a.status;
+                var source   = $('#errore-template').html()
+                var template = Handlebars.compile(source);
+                var context = {numeroErrore: numero, tipoErrore: c}
+                var html = template(context);
+                $('main').append(html);
             }
-            console.log(data);
-            alert('ciao')
-        },
-        error: function(a, b, c){
-            var numero = a.status;
-            var source   = $('#errore-template').html()
-            var template = Handlebars.compile(source);
-            var context = {numeroErrore: numero, tipoErrore: c}
-            var html = template(context);
-            $('main').append(html);
-        }
 
 
-    })
+        })
+
+
+    }
+
 }
 })
