3a892ebb746573b9f03135c4d3c1e701e3b7e40f
maikel abdelmalak <maikel22@hotmail.it>
2020-05-22 15:33:18 +0200

    .

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
 
+$('.search button').on('click', cerca_film ()
+)
+
+
+
+function cerca_film (){
+    var ricerca = 'batman'
+
+    $.ajax({
+
+        'url': 'https://api.themoviedb.org/3/search/movie',
+        'method': 'GET',
+        'data': {
+            'api_key': 'ca530b2f29c17ae553941132a3b631ce',
+            'query': ricerca,
+        },
+        success: function(data){
+            var film = data.results
+            for (var i = 0; i < film.length; i++) {
+                var stampa = film[i].title;
+                console.log(stampa);
+            }
+            console.log(data);
+            alert('ciao')
+        },
+        error: function(a, b, c){
+            var numero = a.status;
+            var source   = $('#errore-template').html()
+            var template = Handlebars.compile(source);
+            var context = {numeroErrore: numero, tipoErrore: c}
+            var html = template(context);
+            $('main').append(html);
+        }
+
+
+    })
+}
 })
